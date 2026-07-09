dd6d72198d8f5cc21b31755eb1cd9deff1744a95
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-09 16:08:22 +0530

    Time: 7 ms (55.62%), Space: 69 MB (84.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,20 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function(nums) {
-    let ans=[]
-    let left=[]
-    let right=[]
-    left[0]=1
-right[nums.length - 1] = 1;
-    for(let i =1 ;  i < nums.length ; i++){
-        left[i]= left[i - 1] * nums[i - 1]
+var productExceptSelf = function (nums) {
+    let ans = []
+    let right = 1;
+    ans[0] = 1
+    right[nums.length - 1] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1]
     }
-     for(let i =nums.length-2;  i >= 0 ; i--){
-        right[i]= right[i + 1] * nums[i+1]
+    console.log(ans)
+    for (let i = nums.length-1 ; i >= 0; i--) {
+        ans[i] *= right;
+        right *= nums[i];
     }
-for (let i = 0; i < nums.length; i++) {
-    ans[i] = left[i] * right[i];
-}
-return ans
+    return ans
 };
