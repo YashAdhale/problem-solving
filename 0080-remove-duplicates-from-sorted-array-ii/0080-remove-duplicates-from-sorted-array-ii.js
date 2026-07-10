05074af3358a8084bf8d789a0fbb0d5d0bf2f11d
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-10 15:16:17 +0530

    Time: 41 ms (98.23%), Space: 57.8 MB (78.99%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var removeDuplicates = function (num) {
-    let left = 2
     if (num.length <= 2) return num.length;
+        let left = 2
+
     for (let i = 2; i < num.length; i++) {
         if (num[i] != num[left-2]) {
             num[left] = num[i]
@@ -13,7 +14,6 @@ var removeDuplicates = function (num) {
         }
         
     }
-    console.log(num)
 
     return left
 };
