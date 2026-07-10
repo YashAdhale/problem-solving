d0ce67bb32ba62f59fd35c3e804f8dce0cdd5495
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-10 15:08:53 +0530

    Time: 56 ms (53.68%), Space: 59.3 MB (19.13%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (num) {
+    let left = 2
+    if (num.length <= 2) return num.length;
+    for (let i = 2; i < num.length; i++) {
+        if (num[i] != num[left-2]) {
+            num[left] = num[i]
+                    left++
+
+        }
+        
+    }
+
+    return left
+};
