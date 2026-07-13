8597a37d657c4bfdcdb49ea2cf9084f9f564e9fe
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-13 11:34:19 +0530

    Time: 2 ms (73.75%), Space: 56.5 MB (32.81%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -6,7 +6,6 @@ var mostWordsFound = function (sentences) {
     let max = 0
     sentences.forEach((sent) => {
        let l= sent.split(" ")
-        console.log(l)
         if (l.length > max) {
             max = l.length
         }
