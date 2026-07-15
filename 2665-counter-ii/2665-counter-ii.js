b7fae30e13c910c730ba315c2a8f2aa9e605f2a5
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-15 18:18:44 +0530

    Time: 44 ms (84.71%), Space: 56.8 MB (41.04%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let o = init
+    return{
+      increment(){
+        return ++init
+      },
+      decrement(){
+        return --init
+      },
+      reset(){
+        return init=o
+      }
+
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
