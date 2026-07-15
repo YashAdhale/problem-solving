7b6e3bc92fcc1e75ff2fe81fbd9b4d4920835687
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-15 12:04:29 +0530

    Time: 1 ms (12.5%), Space: 57.2 MB (75%) - LeetHub

--- 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.ts
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.ts
@@ -1,10 +1,3 @@
 function maxContainers(n: number, w: number, maxWeight: number): number {
-    let totalWeight = (n * n) * w;
-    let container=n*n
-    while (totalWeight > maxWeight && container > 0) {
-        container--; 
-        totalWeight = (container) * w;
-    }
-    
-    return container;
+    return Math.min(n * n, Math.floor(maxWeight / w));
 }
