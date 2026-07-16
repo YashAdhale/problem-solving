4ea29e7a4657efc297eb988e1d9671ea0ed9c2ae
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-16 10:39:17 +0530

    Time: 1 ms (99.77%), Space: 69.2 MB (78.11%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -6,11 +6,10 @@ function findMaxAverage(nums: number[], k: number): number {
     for (let i = 0; i < k; i++) {
         sum += nums[i]
     }
-    maxSum=sum
+    maxSum = sum
     for (let right = k; right < nums.length; right++) {
-        sum -= nums[right - k]
-        sum += nums[right]
-    maxSum=   Math.max(maxSum,sum)
+        sum = sum + nums[right] - nums[right - k]
+        maxSum = Math.max(maxSum, sum)
     }
-    return maxSum/k
+    return maxSum / k
 };
