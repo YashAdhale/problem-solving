8a5c185f861ce8cbf387885bf62d424350c59d8a
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-16 11:18:43 +0530

    Time: 15 ms (78.54%), Space: 57.9 MB (72.68%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -0,0 +1,20 @@
+function maxVowels(s: string, k: number): number {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let current = 0
+    for (let i = 0; i < k; i++) {
+        if (vowels.includes(s[i])) {
+            current++
+        }
+    }
+    if (current == k) {
+        return current
+    }
+    let currentMax = current
+
+    for (let right = k; right < s.length; right++) {
+        if (vowels.includes(s[right - k])) current--;
+        if (vowels.includes(s[right])) current++;
+        if(current>currentMax)currentMax=current
+    }
+    return currentMax
+};
