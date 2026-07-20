0f1b41473515af20994939c878e8ac5518cb20cd
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-20 15:10:06 +0530

    Time: 63 ms (20.83%), Space: 65.2 MB (5.05%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -0,0 +1,17 @@
+function lengthOfLongestSubstring(s: string): number {
+    let obj = {}
+    let left = 0
+    let ans = 0
+    for (let right = 0; right < s.length; right++) {
+
+        obj[s[right]] = obj[s[right]] + 1 || 1
+        while (obj[s[right]] > 1) {
+            obj[s[left]]--;
+            left++;
+        }
+        ans= Math.max(right - left + 1, ans)
+
+    }
+    console.log(obj)
+    return ans
+};
