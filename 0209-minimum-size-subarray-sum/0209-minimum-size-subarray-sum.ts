893b73144fb838c27a3372ec3e09f0eb1012fe29
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-20 12:36:46 +0530

    Time: 3 ms (64.98%), Space: 59 MB (91.66%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
@@ -1,13 +1,14 @@
 function minSubArrayLen(target: number, nums: number[]): number {
-    let left=0
+    let left = 0
     let sum = 0
-    let ans= Infinity
-    for(let right =0 ;right<nums.length;right++){
-    sum += nums[right]
-    while(sum>=target){
-        ans= Math.min(ans, right-left+1)
-        sum-= nums[left]
-        left++
+    let ans = Infinity
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right]
+        while (sum >= target) {
+            ans = Math.min(ans, right - left + 1)
+            sum -= nums[left]
+            left++
+        }
     }
-    }
-return ans === Infinity ? 0 : ans;};
+    return ans === Infinity ? 0 : ans;
+};
