d83071dbbb3ce41fb912d52cc5d2fdf395aabdc6
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-21 12:24:28 +0530

    Time: 133 ms (69.23%), Space: 54 MB (100%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.js
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.js
@@ -1,17 +1,20 @@
-/**
- * @param {number} num
- * @return {boolean}
- */
-var sumOfNumberAndReverse = function(num) {
-    if(num===0){
-        return true
+function reverse(n) {
+    let rev = 0;
+    while (n > 0) {
+        rev = rev * 10 + (n % 10);
+        n = Math.floor(n / 10);
     }
-    for(let i =1 ; i<= num ; i++){
-        let n = i
-        let n1 = Number(i.toString().split('').reverse().join(''));
-        if(n+n1 === num){
-            return true
+    return rev;
+}
+
+var sumOfNumberAndReverse = function(num) {
+    const start = Math.max(0, num - 100000);
+
+    for (let i = start; i <= num; i++) {
+        if (i + reverse(i) === num) {
+            return true;
         }
     }
-    return false
+
+    return false;
 };
