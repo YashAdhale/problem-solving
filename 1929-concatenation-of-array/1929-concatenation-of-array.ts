9a86550415191d841e0bb747901312b3fbc838a0
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-28 17:04:03 +0530

    Time: 657 ms (2.97%), Space: 67.2 MB (5.93%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.ts
@@ -0,0 +1,9 @@
+function getConcatenation(nums: number[]): number[] {
+    let n =nums.length
+  for(let i=0; i<n;i++){
+    nums[(n)+i]=nums[i]
+      console.log( nums)
+
+  }  
+  return nums
+};
