80c108478c12624d83d4f710906ab29a0c137f01
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-07-31 21:02:03 +0530

    Time: 25 ms (66.3%), Space: 60.5 MB (44.27%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.ts
+++ 0076-minimum-window-substring/0076-minimum-window-substring.ts
@@ -9,9 +9,9 @@ function minWindow(s: string, t: string): string {
     let minLength = Infinity
     let left = 0
     for (let i = 0; i < t.length; i++) {
-        needed[t[i]] = needed[t[i]] + 1 || 1
+        needed[t[i]] = needed[t[i]] + 1 || 1 // formed freq map for t
     }
-    let required = Object.keys(needed).length;
+    let required = Object.keys(needed).length; // number unique char in t
 
     for (let right = 0; right < s.length; right++) {
        
@@ -19,25 +19,25 @@ function minWindow(s: string, t: string): string {
         //     window[s[right]] = window[s[right]] + 1 || 1
         // }
 
-        if (needed[s[right]] !== undefined) {
+        if (needed[s[right]] !== undefined) { // if present 
 
-            window[s[right]] = window[s[right]] + 1 || 1
+            window[s[right]] = window[s[right]] + 1 || 1 // add in window
 
-            if (window[s[right]] === needed[s[right]]) {
+            if (window[s[right]] === needed[s[right]]) { // if value in both obj same one char pair completed
                 formed++;
             }
         }
 
-        while (formed === required) {
-            if (right - left + 1 < minLength) {
-                min = s.slice(left, right + 1)
+        while (formed === required) { // after finding answer 
+            if (right - left + 1 < minLength) { // compared current window with min
+                min = s.slice(left, right + 1) // saved min ans till now
                 minLength = min.length
             }
 
             if (needed[s[left]] !== undefined) {
                 window[s[left]]--;
                 if (window[s[left]] < needed[s[left]]) {
-                    formed--;
+                    formed--; // condition will false and we will enter until next time it satisfied if not we have our saved min
                 }
             }
 
