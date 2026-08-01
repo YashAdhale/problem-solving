763c4dec5a7e06ec8ccffe6590d5247af2234bbe
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-08-01 18:37:32 +0530

    Time: 7 ms (34.03%), Space: 62.6 MB (14.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,11 +1,23 @@
 function isPalindrome(s: string): boolean {
-    let cleanText = s.replace(/[^a-zA-Z0-9\s]/g, "")
-   let a = cleanText.toLowerCase().split(" ").join("")
-   let l = a.length-1
-    for(let i =0 ; i<= Math.floor(l/2);i++){
-        if(a[i]!==a[l-i]){
-            return false
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left < right) {
+        if(!isAlphaNumeric(s[right])) {
+            right--
+            continue;
         }
+        if(!isAlphaNumeric(s[left])) {
+            left++
+            continue;
+        }
+        if(s[right].toLowerCase() !== s[left].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
     }
-    return true
+    return true;
 };
+
+const isAlphaNumeric = (c: string) => /[a-z0-9]/i.test(c);
