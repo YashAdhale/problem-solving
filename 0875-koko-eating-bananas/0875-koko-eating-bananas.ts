7bddfd573efafb8cbd66c3b7057491bd90e5072a
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-08-14 17:44:10 +0530

    Time: 6 ms (95.68%), Space: 59.2 MB (67.36%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.ts
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.ts
@@ -3,17 +3,23 @@ function minEatingSpeed(piles: number[], h: number): number {
     let right = Math.max(...piles)
     while (left < right) {
         let mid = Math.floor((left + right) / 2)
-        let hours = 0
-        for (let i = 0; i < piles.length; i++) {
-            hours += Math.ceil(piles[i] / mid)
-        }
-        if (hours<= h) {
+        
+      
+        if (canEatAll(piles,h,mid)) {
             right = mid
         }
-        else if (hours > h) {
+        else {
             left = mid + 1
 
         }
     }
     return left
 };
+
+function canEatAll(piles: number[],h:number,mid:number):boolean{
+       let hours = 0
+       for (let i = 0; i < piles.length; i++) {
+            hours += Math.ceil(piles[i] / mid)
+        }
+        return hours<= h
+}
