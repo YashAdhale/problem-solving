533aa58c3c86d2525ae5f6407c9d76b85e20526d
Yash Adhale <131110594+YashAdhale@users.noreply.github.com>
2026-09-08 16:33:59 +0530

    Time: 602 ms (39.94%), Space: 90.9 MB (35.14%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -1,20 +1,20 @@
-/**
- * @param {string} s
- * @return {string[]}
- */
 var findRepeatedDnaSequences = function (s) {
     let left = 0
-    let map = new Set()
-    // map.add(s.slice(left, left + 10))
+    let seen = new Set()
     let result = new Set()
-    for (let right = 10; right <= s.length; right++) {
-        if (map.has(s.slice(left, right))) {
-            result.add(s.slice(left, right))
-        } else {
-            map.add(s.slice(left, right))
-        }
-        left++
 
+    for (let right = 10; right <= s.length; right++) {
+
+        let sequence = s.slice(left, right)
+
+        if (seen.has(sequence)) {
+            result.add(sequence)
+        } else {
+            seen.add(sequence)
+        }
+
+        left++
     }
+
     return [...result]
-};
+}
